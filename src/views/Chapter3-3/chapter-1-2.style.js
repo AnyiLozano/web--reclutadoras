225debfeff231e42cf9591dc5830a94f1b196f14
anyi lozano <anyi.lozano2119@gmail.com>
2022-03-05 23:06:48 -0500

    merch

--- src/views/Chapter3-3/chapter-1-2.style.js
+++ src/views/Chapter3-3/chapter-1-2.style.js
@@ -79,7 +79,7 @@ export const StyledVideo1 = styled.video`
 
 export const StyledFourImage = styled.img`
   @media screen and (min-width: 325px) and (max-width: 915px) {
-    width: 75%;
+    width: 50%;
   }
 
   @media screen and (min-width: 916px) and (max-width: 1199px) {
